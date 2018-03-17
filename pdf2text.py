19ec8fd256604110810a65c0f0dfed9ceb2faad3
sajan kumar <31013555+ksajan@users.noreply.github.com>
2018-03-17 12:44:18 +0530

    first commit
    changes pdf to text

---
+++ pdf2text.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 22:04:18 2018
+
+@author: sajan kumar
+"""
+
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import os
+import sys, getopt
+
+#converts pdf, returns its text content as a string
+def convert(random, pages=4):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = file(random, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    print(text)
